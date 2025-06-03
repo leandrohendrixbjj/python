a73672bf9b200b2f11c7708e727c2cf7052f474f
leandro starks <leandrotully@gmail.com>
2025-06-03 19:21:08 -0300

    Métodos Phyton

--- projetos/poo-sabor-express/modelos/restaurante.py
+++ projetos/poo-sabor-express/modelos/restaurante.py
@@ -1,16 +1,32 @@
 class Resturante:
-  nome = None
-  categoria = None
-  situacao = False
 
+  data = []
 
-res = Resturante()
-res.nome = "Pizzaria do João"
-res.categoria = "Pizzaria"
-res.situacao = True
+  def __init__(self,nome, categoria, situacao):
+    self.nome = nome 
+    self.categoria = categoria
+    self.situacao = situacao
+    Resturante.data.append(self)
 
-# Exibindo os métodos e atributos do objeto
-print(dir(res))  
+  def __str__(self):
+    return f'{self.nome} - {self.categoria} - {self.situacao}'  
+  
+  def list():
+    for data in Resturante.data:
+      print(f'{data.nome} - {data.categoria} - {data.situacao}')
+
+  
+
+res = Resturante('Pizzaria do João', 'Pizzaria', 'Aberto')
+res_1 = Resturante('Churrascaria do Zé', 'Churrascaria', 'Fechado')
+
+Resturante.list()
+
+#print(res.__str__())  
+
 
 # Exibindo os atributos do objeto
-print(vars(res))
+#print(vars(res))
+
+# Exibindo os métodos e atributos do objeto
+#print(dir(res))  
