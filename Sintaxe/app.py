ac3653321fb39944346f4ffea09a63449f425822
leandro starks <leandrotully@gmail.com>
2025-06-11 09:37:22 -0300

    MethodsClasse

--- Sintaxe/app.py
+++ Sintaxe/app.py
@@ -4,20 +4,26 @@ clear.clear()
 
 class Pessoa:
   def __init__(self, name,age):
-    self.name = name
-    self.age = age   
-    self._active = True
+    self.name = name.title()
+    self.age = age    
 
-"""
-  Property: sinaliza ao desenvolvedor (ou a si mesmo no futuro) que esse atributo:
-    1- Não deve ser acessado diretamente fora da classe
-    2- Pode estar sujeito a mudança
-    3- Pode ter lógica especial envolvida no acesso (como uma propriedade @property depois)  
-"""
-@property
-def active(self):
-  return 'True' if self._active else 'False'   
-  
-pessoa = Pessoa('leandro',40)
+  """
+    classmethod recebe a classe como primeiro argumento, em vez da instância
+     1-) Por convenção, chamamos esse primeiro parâmetro de cls (assim como usamos self para instâncias).
+     2-) Ele pode ser usado para acessar ou modificar atributos da classe, ou para criar instâncias alternativas.
 
-print(vars(pessoa))
+    Quando usar:
+      1-) Precisa acessar ou modificar atributos da classe
+      2-) Quer criar instâncias alternativas (como fábricas)
+      3-) Quer um método que não dependa de uma instância específica 
+  """   
+  @classmethod
+  def criar_bebe(cls,nome):
+    return cls(nome,0)
+ 
+adulto = Pessoa('leandro',40)
+print(vars(adulto))
+
+bebe = Pessoa.criar_bebe('Luna')
+print(vars(bebe))
+
--- projetos/poo-sabor-express/modelos/restaurante.py
+++ projetos/poo-sabor-express/modelos/restaurante.py
@@ -2,19 +2,36 @@ class Resturante:
 
   data = []
 
+  """
+     Todos os atributos que possuem "_" indica que seu valor deve ser alterado dentro da classe. 
+     Isso é recomendado por convesão.
+     
+     Ele não impede que o atributo seja alterado fora da classe, é uma indicação ao DEV 
+     que isso não deve ser feito. Por convesão todo atributo deve ter o "_"
+
+  """
   def __init__(self,nome, categoria):
-    self.nome = nome 
-    self.categoria = categoria
+    self._nome = nome.title() 
+    self._categoria = categoria.upper()
     self._ativo = False
     Resturante.data.append(self)
 
   def __str__(self):
-    return f'{self.nome} - {self.categoria} - {self._ativo}'  
+    return f'{self._nome} - {self._categoria} - {self._ativo}'  
   
-  def list():
+  """
+    classmethod recebe a classe como primeiro argumento, em vez da instância
+     1-) Por convenção, chamamos esse primeiro parâmetro de cls (assim como usamos self para instâncias).
+     2-) Ele pode ser usado para acessar ou modificar atributos da classe, ou para criar instâncias alternativas.
+  """  
+  @classmethod
+  def list(cls):
     print(f'{"Nome":<25} {"Categoria":<25} {"Situação":<5}')
     for data in Resturante.data:
-      print(f'{data.nome.ljust(25)} {data.categoria.ljust(25)} {data._ativo}')  
+      print(f'{data._nome.ljust(25)} {data._categoria.ljust(25)} {data._ativo}')  
+
+  def update_ativo(self):
+    self._ativo = not self._ativo
 
   """
   Property: sinaliza ao desenvolvedor (ou a si mesmo no futuro) que esse atributo:
@@ -26,8 +43,10 @@ class Resturante:
   def ativo(self):
     return "True" if self._ativo else "False"
 
-res = Resturante('Pizzaria do João', 'Pizzaria')
-res_1 = Resturante('Churrascaria do Zé', 'Churrascaria')
+res = Resturante('pizzaria do João', 'Pizzaria')
+res.update_ativo()
+
+res_1 = Resturante('churrascaria do Zé', 'Churrascaria')
 
 Resturante.list()
 
