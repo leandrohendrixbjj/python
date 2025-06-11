d273c38ef85b258c2231630f365fe7817ed1debf
leandro starks <leandrotully@gmail.com>
2025-06-11 09:01:20 -0300

    Classes

--- projetos/poo-sabor-express/modelos/restaurante.py
+++ projetos/poo-sabor-express/modelos/restaurante.py
@@ -2,23 +2,32 @@ class Resturante:
 
   data = []
 
-  def __init__(self,nome, categoria, situacao):
+  def __init__(self,nome, categoria):
     self.nome = nome 
     self.categoria = categoria
-    self.situacao = situacao
+    self._ativo = False
     Resturante.data.append(self)
 
   def __str__(self):
-    return f'{self.nome} - {self.categoria} - {self.situacao}'  
+    return f'{self.nome} - {self.categoria} - {self._ativo}'  
   
   def list():
+    print(f'{"Nome":<25} {"Categoria":<25} {"Situação":<5}')
     for data in Resturante.data:
-      print(f'{data.nome} - {data.categoria} - {data.situacao}')
+      print(f'{data.nome.ljust(25)} {data.categoria.ljust(25)} {data._ativo}')  
 
-  
+  """
+  Property: sinaliza ao desenvolvedor (ou a si mesmo no futuro) que esse atributo:
+    1- Não deve ser acessado diretamente fora da classe
+    2- Pode estar sujeito a mudança
+    3- Pode ter lógica especial envolvida no acesso (como uma propriedade @property depois)  
+  """
+  @property
+  def ativo(self):
+    return "True" if self._ativo else "False"
 
-res = Resturante('Pizzaria do João', 'Pizzaria', 'Aberto')
-res_1 = Resturante('Churrascaria do Zé', 'Churrascaria', 'Fechado')
+res = Resturante('Pizzaria do João', 'Pizzaria')
+res_1 = Resturante('Churrascaria do Zé', 'Churrascaria')
 
 Resturante.list()
 
